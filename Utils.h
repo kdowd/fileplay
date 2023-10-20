6333c50d7b21c8252a1d6373ab0c17bb4f360a79
091275 <091275@CIT3-205-01>
2023-10-20 13:02:25 +1300

    Add project files.

---
+++ Utils.h
@@ -0,0 +1,16 @@
+#pragma once
+#include <iostream> 
+#include <string> 
+#include <queue>
+#include <windows.h>
+
+
+
+void print_queue(std::queue<int> q) {
+
+	while (!q.empty()) {
+		std::cout << q.front() << std::endl;
+		Sleep(1000);
+		q.pop();
+	}
+}
---
+++ fileplay.cpp
@@ -0,0 +1,23 @@
+#include <iostream> 
+#include <string> 
+#include <queue>
+#include <windows.h>
+#include "Utils.h"
+
+using namespace std;
+
+
+
+int main()
+
+{
+	queue<int> myQueue;
+
+	myQueue.push(99);
+	myQueue.push(108);
+	myQueue.push(9);
+	myQueue.push(43);
+	print_queue(myQueue);
+
+	return 0;
+}
